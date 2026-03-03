2659229073ee6588cffb45ea8364c519c67de011
ManojKumar <manojkumarb134@gmail.com>
2026-03-03 16:29:04 +0530

    exploring get post call

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
-
+// order of the routes matters, the first matching route will be executed, so if we put the '/' route before the others, it will always match and the other routes will never be reached. 
+// app.use is used to define a route that responds to all HTTP methods (GET, POST, etc.) and can be used for middleware or for defining routes.
 app.use('/test',(req, res)=>{
     res.send('Hello from the test route!23');
 })
-app.use('/',(req, res)=>{
-    res.send('empty path!!!');
-})
+// app.get is used to define a route that responds only to GET requests.
+app.get('/user',(req, res)=>{
+    res.send({name: 'John', age: 30});
+});
+// app.post is used to define a route that responds only to POST requests.
+app.post('/user',(req, res)=>{
+    res.send('User created successfully!');
+});
+// app.use('/',(req, res)=>{
+//     res.send('empty path!!!');
+// })
+
 app.listen(3000,()=>{ // callback function to run when the server starts
     console.log('Server is running on port 3000');
 })
