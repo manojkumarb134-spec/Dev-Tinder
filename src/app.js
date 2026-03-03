cd1fdb75533b6d8ad07ab555a762cc2212510ff0
ManojKumar <manojkumarb134@gmail.com>
2026-03-03 14:38:20 +0530

    add server

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ app.use('/test',(req, res)=>{
     res.send('Hello from the test route!23');
 })
 app.use('/',(req, res)=>{
-    res.send('empty path!!');
+    res.send('empty path!!!');
 })
 app.listen(3000,()=>{ // callback function to run when the server starts
     console.log('Server is running on port 3000');
